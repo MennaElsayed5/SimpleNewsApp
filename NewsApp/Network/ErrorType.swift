80e828d1173d7ab77ea42dc3b12ef7cc221317bf
MennaElsayed5 <mennaaaaelsayed@gmail.com>
2022-09-20 19:17:26 +0200

    building the network layer to calling api

---
+++ NewsApp/Network/ErrorType.swift
@@ -0,0 +1,16 @@
+//
+//  ErrorType.swift
+//  NewsApp
+//
+//  Created by Menna on 20/09/2022.
+//
+
+import Foundation
+
+enum ErrorType:Error {
+    
+    case InternalError
+    case ServerError
+    case parsingError
+    case urlBadFormmated
+}
---
+++ NewsApp/Network/Methods.swift
@@ -0,0 +1,16 @@
+//
+//  Methods.swift
+//  NewsApp
+//
+//  Created by Menna on 20/09/2022.
+//
+
+import Foundation
+
+enum Methods {
+
+    case GET
+    case POST
+    case PUT
+    case DELETE
+}
