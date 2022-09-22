4291e9388fd67da7317838bf6439aa081d7f20ec
MennaElsayed5 <mennaaaaelsayed@gmail.com>
2022-09-22 03:32:46 +0200

    check connection in home and search screens with handling and refactoring code in home screen

---
+++ NewsApp/Utilites/HandleConnection.swift
@@ -0,0 +1,44 @@
+//
+//  HandleConnection.swift
+//  NewsApp
+//
+//  Created by Menna on 22/09/2022.
+//
+
+import Foundation
+import Reachability
+class HandelConnection{
+    static let handelConnection = HandelConnection()
+    var reachability: Reachability?
+    func checkNetworkConnection(complition: @escaping (Bool)-> Void){
+        reachability = try! Reachability()
+        guard let reachability = reachability else {return}
+        reachability.whenReachable = { reachability in
+            if reachability.connection == .wifi {
+                print("Reachable via WiFi")
+                complition(true)
+            } else {
+                print("Reachable via Cellular")
+                complition(true)
+            }
+        }
+        reachability.whenUnreachable = { _ in
+            print("Not reachable")
+            complition(false)
+        }
+        do {
+            try reachability.startNotifier()
+        } catch {
+            print("Unable to start notifier")
+        }
+    }
+
+}
+extension UIViewController{
+    func showAlertForInterNetConnection(){
+        let alert = UIAlertController(title: "network is not connected", message: "please, check your internet connection", preferredStyle: .alert)
+        let okBtn = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+        alert.addAction(okBtn)
+        self.present(alert, animated: true, completion: nil)
+    }
+}
--- NewsApp/Views/Home/ListNewsViewController.swift
+++ NewsApp/Views/Home/ListNewsViewController.swift
@@ -7,46 +7,77 @@
 
 import UIKit
 import RxSwift
-import Kingfisher
 class ListNewsViewController: UIViewController {
     var countryName:String?
     var catgory:String?
-    var tabBarView:TabBarViewController?
     @IBOutlet weak var newsTb: UITableView!
-    var newsViewModel: NewsViewModel?
-    let disBag = DisposeBag()
-    var arrayOfArticle: [Article] = []
-    var articles : [Articles] = []
-    var artile:Article?
-    var isFav : Bool?
+     var newsViewModel: NewsViewModel?
+     let disBag = DisposeBag()
+     var arrayOfArticle: [Article] = []
+     var articles : [Articles] = []
+     var artile:Article?
+     var isFav : Bool?
+   private  var isConn:Bool = false
+   private  let refreshController = UIRefreshControl()
+    @IBOutlet weak var notConnectionView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("naamm\(countryName)naaaaaam\(catgory)")
+    //    print("naamm\(countryName)naaaaaam\(catgory)")
         newsViewModel = NewsViewModel(appDelegate: ((UIApplication.shared.delegate as? AppDelegate)!))
         self.newsTb.delegate=self
         self.newsTb.dataSource=self
         let newsCell = UINib(nibName: "NewsTableViewCell", bundle: nil)
         newsTb.register(newsCell, forCellReuseIdentifier: "NewsTableViewCell")
-        fetchData()
+        refreshController.addTarget(self, action: #selector(pullToRefresh), for: .valueChanged)
+        newsTb.addSubview(refreshController)
         // Do any additional setup after loading the view.
     }
     override func viewWillAppear(_ animated: Bool) {
+        checkNetwork()
         getArticlesFromCoreData()
 
     }
     func fetchData(){
         newsViewModel?.getNewsFromApi(countryName:countryName ?? "us", catgoryId: catgory ?? "general")
-    //    newsViewModel?.allNews()
         newsViewModel?.newsObservable.subscribe(on: ConcurrentDispatchQueueScheduler.init(qos: .background))
             .observe(on: MainScheduler.asyncInstance)
             .subscribe { news in
                 self.arrayOfArticle = news
-              //  self.artile=news.element[]
                 print("newwws\(news.count)")
                 self.newsTb.reloadData()
             } onError: { error in
                 print(error)
             }.disposed(by: disBag)
+    }
+    func checkNetwork() {
+        newsViewModel?.checkConnection()
+        newsViewModel?.networkObservable.subscribe {[weak self] isConn in
+            self?.isConn = isConn
+            if isConn == false{
+                self?.notConnectionView.isHidden = false
+            }else{
+                self?.notConnectionView.isHidden = true
+                self?.fetchData()
+            }
+        } onError: { error in
+            print("connection error network")
+        } onCompleted: {
+            print("onComplete network")
+        } onDisposed: {
+            print("ondispose network")
+        }.disposed(by: disBag)
+
+    }
+    @objc func pullToRefresh(){
+        refreshController.beginRefreshing()
+        checkNetwork()
+        DispatchQueue.main.asyncAfter(deadline: .now()+5) {
+            if self.refreshController.isRefreshing{
+                self.refreshController.endRefreshing()
+            }
+        }
+        
     }
     /*
     // MARK: - Navigation
@@ -57,71 +88,38 @@ class ListNewsViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
-    @objc private func longPress(recognizer: UIButton) {
-        newsViewModel?.checkNewsInCoreData(data: "\(arrayOfArticle[recognizer.tag].publishedAt ?? "date")", auther: "\(arrayOfArticle[recognizer.tag].author ?? "auther")")
-            if newsViewModel?.isFav == false {
-                do{
-                    try newsViewModel?.saveoCoreData(title: arrayOfArticle[recognizer.tag].title ?? "title", img: arrayOfArticle[recognizer.tag].urlToImage ?? "news", desc:arrayOfArticle[recognizer.tag].description ?? "des", source: arrayOfArticle[recognizer.tag].source?.name ?? "source", data: arrayOfArticle[recognizer.tag].publishedAt ?? "2022/9/23", auther: arrayOfArticle[recognizer.tag].author ?? "auther", completion: { [self]  result in
-                    switch result{
-                    case true:
-                        print("arrayOfArticle[recognizer.tag].title \(arrayOfArticle[recognizer.tag].title)")
-                        print("arrayOfArticle[recognizer.tag].title \(arrayOfArticle[recognizer.tag].source?.name)")
 
-                    print("add to core ")
-                        case false :
-                        print("faild to add to core")
-                                }
-                                })
-                            }
-
-                    catch let error{
-                print(error.localizedDescription)
-                                  }
-                recognizer.setImage(UIImage(systemName: "heart.fill"), for : UIControl.State.normal)
-            }
-            else{
-                print("in core data ")
-                recognizer.setImage(UIImage(systemName: "heart"), for : UIControl.State.normal)
-
-            }
-
-
-      }
+}
+extension ListNewsViewController{
     func getArticlesFromCoreData(){
         do{
             try  newsViewModel?.getAllnewsInCoreData(completion: { response in
                 //MARK: LSA M5LST4
                 switch response{
                 case true:
-//                    self.emptyView.isHidden=true
                     print("data retrived successfuly")
                 case false:
                     print("data cant't retrieved")
                 }
             })
-            
         }
         catch let error{
             print(error.localizedDescription)
         }
         articles = (newsViewModel?.articleList)!
-        print("arrrry\(arrayOfArticle)")
         newsTb.reloadData()
             }
     func deleteItemFromCoreData(index:IndexPath){
         do{
             try self.newsViewModel?.removeNewsFromCoreDatat(title: "\(arrayOfArticle[index.row].title ?? "title")", completionHandler: { result in
                 switch result{
-                                case true:
-                                    print("remove from cart")
-                                   self.getArticlesFromCoreData()
-                                   self.newsTb.reloadData()
-                                   // if self.CartProducts.count == 0 {
-                                    //self.emptyView.isHidden=false
+                case true:
+                print("remove from cart")
+                self.getArticlesFromCoreData()
+                self.newsTb.reloadData()
                 case false:
-                    print("")
-                                    }
-                                
+                    print("cann't delet")
+                        }
             })
         }
         catch let error{
@@ -133,86 +131,8 @@ class ListNewsViewController: UIViewController {
         let alert = UIAlertController(title: "Already Saved", message: "Are You remove this item from the Fav", preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "Cancel", style: .default, handler: nil))
         alert.addAction(UIAlertAction(title: "OK", style: .destructive, handler: { [self] UIAlertAction in
-                       self.deleteItemFromCoreData(index: indexPath)
+        self.deleteItemFromCoreData(index: indexPath)
         }))
         self.present(alert, animated: true, completion: nil)
     }
-    func setupBtn(btn:UIButton){
-        if newsViewModel?.isFav == false {
-            btn.setImage(UIImage(systemName: "heart"), for : UIControl.State.normal)
-        }else{
-            btn.setImage(UIImage(systemName: "heart.fill"), for : UIControl.State.normal)
-        }
-    }
-}
-extension ListNewsViewController: UITableViewDelegate,UITableViewDataSource{
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        print("arrayOfArticle.count\(arrayOfArticle.count)")
-        return arrayOfArticle.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "NewsTableViewCell") as! NewsTableViewCell
-        let url = URL(string: arrayOfArticle[indexPath.row].urlToImage ?? "news")
-        let processor = DownsamplingImageProcessor(size: cell.imgNews.bounds.size)
-                     |> RoundCornerImageProcessor(cornerRadius: 20)
-        cell.newsTitle.text = arrayOfArticle[indexPath.row].title
-        cell.imgNews.kf.indicatorType = .activity
-        cell.imgNews.kf.setImage(
-            with: url,
-            options: [
-                .processor(processor),
-                .scaleFactor(UIScreen.main.scale),
-                .transition(.fade(1)),
-                .cacheOriginalImage
-            ])
-        cell.imgNews.layer.borderWidth = 1
-        cell.imgNews.layer.borderColor = UIColor.lightGray.cgColor
-        cell.imgNews.layer.cornerRadius = 20
-        cell.shortDescription.text=arrayOfArticle[indexPath.row].description
-        cell.source.text = arrayOfArticle[indexPath.row].source?.name
-        cell.date.text = arrayOfArticle[indexPath.row].publishedAt
-       // setupBtn(btn: cell.favBtn)
-       // cell.tag = indexPath.row
-        cell.addToFav={ [self] in
-            newsViewModel?.checkNewsInCoreData(data: arrayOfArticle[indexPath.row].publishedAt ?? "title", auther: arrayOfArticle[indexPath.row].author ?? "auther")
-            isFav = newsViewModel?.isFav
-            if (isFav!) {
-                self.showDeleteAlert(indexPath: indexPath)
-                print("alreadyIsFav\(isFav)")
-            }else{
-                do{
-                    try newsViewModel?.saveoCoreData(title: arrayOfArticle[indexPath.row].title ?? "title", img: arrayOfArticle[indexPath.row].urlToImage ?? "news", desc:arrayOfArticle[indexPath.row].description ?? "des", source: arrayOfArticle[indexPath.row].source?.name ?? "source", data: arrayOfArticle[indexPath.row].publishedAt ?? "2022/9/23", auther: arrayOfArticle[indexPath.row].author ?? "auther", completion: {  result in
-                          switch result{
-                          case true:
-                              print("add to core ")
-                          case false :
-                              print("faild to add to core")
-                          }
-                      })
-                  }
-
-              catch let error{
-                      print(error.localizedDescription)
-                  }
-
-            }
-
-
-        }
-  
-   //   cell.favBtn.addTarget(self, action: #selector(longPress(recognizer:)), for: .touchUpInside)
-   //    cell.favBtn.accessibilityValue=arrayOfArticle[indexPath.row].title
-        return cell
-    }
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat{
-            return 230
-       }
-       func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-           return 60
-       }
-    
 }
